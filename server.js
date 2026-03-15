aeb9ca70f517525ee950d5b8424cd7dd9ccce4ec
Akhilgudipudi <2400030824@kluniversity.in>
2026-03-15 15:44:01 +0530

    Refactor server.js for improved code execution

--- server.js
+++ server.js
@@ -1,121 +1,124 @@
-{variant=“standard” id=“serverjs-full-updated”}
+{variant=“standard” id=“serverjs-complete-final”}
 const express = require(“express”)
 const fs = require(“fs”)
-const { exec } = require(“child_process”)
 const path = require(“path”)
+const { exec } = require(“child_process”)
 
 const app = express()
 
 app.use(express.json())
 app.use(express.static(“public”))
 
-const codesDir = path.join(__dirname,“codes”)
+const codesDir = path.join(__dirname, “codes”)
 
-if(!fs.existsSync(codesDir)){
+// Create codes folder if not exists
+if (!fs.existsSync(codesDir)) {
 fs.mkdirSync(codesDir)
 }
 
-app.post(”/run”,(req,res)=>{
-
-const {code,language,input} = req.body
+app.post(”/run”, (req, res) => {
+const { code, language, input } = req.body
 
 let filePath
-let compileCmd
+let compileCmd = null
 let runCmd
 
-if(language===“python”){
+// PYTHON
+if (language === "python") {
 
-filePath = path.join(codesDir,“main.py”)
+    filePath = path.join(codesDir, "main.py")
 
-fs.writeFileSync(filePath,code)
+    fs.writeFileSync(filePath, code)
 
-runCmd = python ${filePath}
-
-}
-
-else if(language===“c”){
-
-filePath = path.join(codesDir,“main.c”)
-
-fs.writeFileSync(filePath,code)
-
-compileCmd = gcc ${filePath} -o ${codesDir}/main.exe
-
-runCmd = ${codesDir}/main.exe
+    runCmd = `python "${filePath}"`
 
 }
 
-else if(language===“cpp”){
+// C
+else if (language === "c") {
 
-filePath = path.join(codesDir,“main.cpp”)
+    filePath = path.join(codesDir, "main.c")
 
-fs.writeFileSync(filePath,code)
+    fs.writeFileSync(filePath, code)
 
-compileCmd = g++ ${filePath} -o ${codesDir}/main.exe
+    compileCmd = `gcc "${filePath}" -o "${codesDir}/main.exe"`
 
-runCmd = ${codesDir}/main.exe
-
-}
-
-else if(language===“java”){
-
-filePath = path.join(codesDir,“Main.java”)
-
-fs.writeFileSync(filePath,code)
-
-compileCmd = javac ${filePath}
-
-runCmd = java -cp ${codesDir} Main
+    runCmd = `"${codesDir}/main.exe"`
 
 }
 
-function executeRun(){
+// C++
+else if (language === "cpp") {
 
-const process = exec(runCmd,(err,stdout,stderr)=>{
+    filePath = path.join(codesDir, "main.cpp")
 
-if(err){
-return res.json({output:err.toString()})
-}
+    fs.writeFileSync(filePath, code)
 
-if(stderr){
-return res.json({output:stderr})
-}
+    compileCmd = `g++ "${filePath}" -o "${codesDir}/main.exe"`
 
-res.json({output:stdout})
-
-})
-
-if(input){
-process.stdin.write(input)
-process.stdin.end()
-}
+    runCmd = `"${codesDir}/main.exe"`
 
 }
 
-if(compileCmd){
+// JAVA
+else if (language === "java") {
 
-exec(compileCmd,(compileErr)=>{
+    filePath = path.join(codesDir, "Main.java")
 
-if(compileErr){
-return res.json({output:compileErr.toString()})
-}
+    fs.writeFileSync(filePath, code)
 
-executeRun()
+    compileCmd = `javac "${filePath}"`
 
-})
-
-}else{
-
-executeRun()
+    runCmd = `java -cp "${codesDir}" Main`
 
 }
 
+function runProgram() {
+
+    const program = exec(runCmd, (error, stdout, stderr) => {
+
+        if (error) {
+            return res.json({ output: error.toString() })
+        }
+
+        if (stderr) {
+            return res.json({ output: stderr })
+        }
+
+        res.json({
+            output: stdout || "Program executed successfully with no output"
+        })
+
+    })
+
+    if (input) {
+        program.stdin.write(input)
+        program.stdin.end()
+    }
+
+}
+
+if (compileCmd) {
+
+    exec(compileCmd, (compileError) => {
+
+        if (compileError) {
+            return res.json({ output: compileError.toString() })
+        }
+
+        runProgram()
+
+    })
+
+} else {
+
+    runProgram()
+
+}
 })
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT,()=>{
-
-console.log(“🚀 Akhil Compiler Server Running”)
-
+app.listen(PORT, () => {
+console.log(“🚀 Akhil Compiler Backend Running on Port “ + PORT)
 })
